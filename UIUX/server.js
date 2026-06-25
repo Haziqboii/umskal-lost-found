331edea6a964fd7178e82bed223e534468823636
Haziqboii <haxim.2mars@gmail.com>
2026-06-25 14:25:54 +0800

    Fix: Configure port to bind dynamically to process.env.PORT for Render deployment

--- UIUX/server.js
+++ UIUX/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
